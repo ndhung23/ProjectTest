1774303c06dda86078f08c23e2d7adcb176dba87
Zoxy <139949025+ndhung23@users.noreply.github.com>
2026-01-02 14:31:23 +0700

    chuan bi deploy

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,39 @@
-import express, { request, response } from 'express';
-import tasksRouters from './routers/tasksRouters.js'; 
-import { connectDB } from './src/config/db.js';
-import dotenv from 'dotenv';
-import cors from 'cors';//cn backend
+import express, { request, response } from "express";
+import tasksRouters from "./routers/tasksRouters.js";
+import { connectDB } from "./src/config/db.js";
+import dotenv from "dotenv";
+import cors from "cors"; //cn backend
+import path from "path";
+
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
+
+//git
+const __dirname = path.resolve();
 
 //middlewares
 app.use(express.json());
-//cn backend (2)
-app.use(cors({origin: ["http://localhost:5173",
-                       "http://abc.com"
-]}))
 
+//cn backend (2):dung de chay tren moi truong development
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: ["http://localhost:5173", "http://abc.com"] }));
+}
 
-app.use("/api/tasks",tasksRouters);
+app.use("/api/tasks", tasksRouters);
+
+//git
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
 //Ket noi databe xong moi chay o cong 5001
 connectDB().then(() => {
-    app.listen(PORT,() =>{
-        console.log("Server start with port ${PORT}");
-    });
+  app.listen(PORT, () => {
+    console.log("Server start with port ${PORT}");
+  });
 });
-
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,12 @@
 import axios from "axios";
+//dung git free no cho url ngau nhien
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 
-const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+// const BASE_URL =
+//   import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
-});
+}); 
 
 export default api;
